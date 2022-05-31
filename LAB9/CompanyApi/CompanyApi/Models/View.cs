c19ac0f247e97c48b10f0d0c6938041f5afd07af
VoiTee <wuriakpl@gmail.com>
2022-06-01 01:14:59 +0200

    LAB9

---
+++ LAB9/CompanyApi/CompanyApi/Models/View.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyApi.Models
+{
+    [Keyless]
+    public partial class View
+    {
+        public int EmployeeId { get; set; }
+        [StringLength(15)]
+        [Unicode(false)]
+        public string? FirstName { get; set; }
+        [StringLength(15)]
+        [Unicode(false)]
+        public string? LastName { get; set; }
+        public int? ManagerId { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal? Salary { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal? Bonus { get; set; }
+        public int? DepartmentId { get; set; }
+    }
+}
